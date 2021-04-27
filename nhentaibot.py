fc23d8b09a92ef941d96f696dab3f041406d2100
Young-TW <young20050727@gmail.com>
2021-04-27 22:34:11 +0800

    update  hentai bot

--- nhentaibot.py
+++ nhentaibot.py
@@ -16,41 +16,46 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=game)
 ##################################################################################################
 @bot.command()
-async def n(ctx,number=None,page=0):
-    if number!=None:
-        text = f"https://nhentai.net/g/{number}/"
-        #爬蟲
-        hentai = requests.get(text)
-        data = bs4.BeautifulSoup(hentai.text, "lxml").select("#thumbnail-container img")
-        urls = [i["src"] for i in data if not i["src"].startswith("data")]
-        #輸出訊息
-        embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=urls[0])
-        embed.set_footer(text=" By Young#0001")
-        embed.set_image(url=urls[0])
-        message=await ctx.send(embed=embed)
-        for i in ["◀","▶"]:
-            await message.add_reaction(i)
-        #檢查表情符號(函式)
-        def check(reaction, user):
-            return user == ctx.author and reaction.message == message
-        #檢查表情符號(迴圈)
-        while 1 :
-            if(page + 1 > len(urls) - 1):
-                embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",description="The end.")
+async def n(ctx,channel,number=None,page=0):
+    channel = n.channel
+    c = channel.is_nsfw(channel)
+    if c == 1:
+        if number!=None:
+            text = f"https://nhentai.net/g/{number}/"
+            #爬蟲
+            hentai = requests.get(text)
+            data = bs4.BeautifulSoup(hentai.text, "lxml").select("#thumbnail-container img")
+            urls = [i["src"] for i in data if not i["src"].startswith("data")]
+            #輸出訊息
+            embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=text)
+            embed.set_footer(text=" By Young#0001")
+            embed.set_image(url=urls[0])
+            message=await ctx.send(embed=embed)
+            for i in ["◀","▶"]:
+                await message.add_reaction(i)
+            #檢查表情符號(函式)
+            def check(reaction, user):
+                return user == ctx.author and reaction.message == message
+            #檢查表情符號(迴圈)
+            while 1 :
+                if(page + 1 > len(urls) - 1):
+                    embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",description="The end.")
+                    embed.set_footer(text="By Young#0001")
+                    await message.edit(embed=embed)
+                    break
+                reaction, user = await bot.wait_for("reaction_add",timeout=60.0,check=check)
+                if str(reaction) ==  "▶":
+                    page+=1
+                elif str(reaction) == "◀":
+                    page-=1
+                await message.remove_reaction(reaction,user)
+                embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=text)
                 embed.set_footer(text="By Young#0001")
+                embed.set_image(url=urls[page])
                 await message.edit(embed=embed)
-                break
-            reaction, user = await bot.wait_for("reaction_add",timeout=60.0,check=check)
-            if str(reaction) ==  "▶":
-                page+=1
-            elif str(reaction) == "◀":
-                page-=1
-            await message.remove_reaction(reaction,user)
-            embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=f"{urls[page]}")
-            embed.set_footer(text="By Young#0001")
-            embed.set_image(url=f"{urls[page]}")
-            await message.edit(embed=embed)
+        else:
+            await ctx.send(f"Please input number")   
     else:
-        await ctx.send(f"請輸入參數")
+        ctx.send("This is not NSFW channel")
 ##################################################################################################
 bot.run('Nzg2OTg2OTU0NDc4NTgzODU4.X9OYtw.XhQYPe-aMEeNpHbORtQdbboflQg')
