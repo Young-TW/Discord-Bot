4375f108e14331eb5d75b65ff6d424789aebea25
Young-TW <young20050727@gmail.com>
2021-07-01 03:26:51 +0800

    1.2 done

--- nhentaibot1.2.py
+++ nhentaibot1.2.py
@@ -5,6 +5,7 @@ import copy
 import requests
 import bs4
 import json
+from bs4 import BeautifulSoup
 
 with open('setting.json', mode='r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -17,6 +18,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     game = discord.Game("Testing")
     await bot.change_presence(status=discord.Status.idle, activity=game)
+
 ##################################################################################################
 
 @bot.command()
@@ -25,35 +27,41 @@ async def n(ctx,number=None,page=0):
     if c == False:
         await ctx.send("This is not NSFW channel")
     else:
+        urls = []
+        def saveUrlIndex(index):
+            urls.append(index)
         if number!=None:
             #爬蟲
             #從內容頁面
-            urls = []
             page = 0
             while 1:
-                text = f"https://nhentai.net/g/{number}/{page+1}/"
-                hentai = requests.get(text)
-                #hentai.encoding = 'utf-8'
-                data = bs4.BeautifulSoup(hentai.text, "lxml").select("#image-container img")#這行不確定
-
-                urls = ["src"] if not ["src"].startswith("src")
-
-                strTitle = bs4.BeautifulSoup(hentai.text, "lxml").bs4.soup.title.text#這行也不確定
-                if ("404" in strTitle) == True:#判斷網頁是否404
+                print(f"第{page+1}圈爬蟲")
+                url = f"https://nhentai.net/g/{number}/{page+1}/"
+                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362'}
+                r = requests.get(url,headers=headers)
+                Burl = BeautifulSoup(r.text, 'html.parser')
+                img_tags = Burl.find_all('img')
+                for tag in img_tags:
+                    imgUrl = tag.get('src')
+                print(f"imgUrl: {imgUrl}")
+                saveUrlIndex(imgUrl)
+                #判斷網頁是否結束
+                if imgUrl == "https://static.nhentai.net/img/logo.090da3be7b51.svg":
                     break
-                #urls[page] = f"{data}"
-                #if not i["src"].startswith("data")
+
                 page += 1
-                #img class=fit-horizontal
-                #section id=image-container
+
+            page = 0
             #輸出訊息
-            embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=text)
+            print("輸出訊息")
+            embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=url)
             embed.set_footer(text="By Young#0001")
             embed.set_image(url=urls[0])
             message=await ctx.send(embed=embed)
             for i in ["◀","▶"]:
                 await message.add_reaction(i)
             #檢查表情符號(函式)
+            print("檢查表情符號")
             def check(reaction, user):
                 return user == ctx.author and reaction.message == message
             #檢查表情符號(迴圈)
@@ -69,7 +77,7 @@ async def n(ctx,number=None,page=0):
                 elif str(reaction) == "◀":
                     page-=1
                 await message.remove_reaction(reaction,user)
-                embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=text)
+                embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",url=url)
                 embed.set_footer(text="By Young#0001")
                 embed.set_image(url=urls[page])
                 await message.edit(embed=embed)
