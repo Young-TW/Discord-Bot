af4fde7f3463090bce31f30f5f57efe6f2ced042
Young <young20050727@gmail.com>
2021-09-28 05:49:09 +0800

    不會寫多執行緒QQ

--- nhentai/nhentaibot2.0.py
+++ nhentai/nhentaibot2.0.py
@@ -1,15 +1,11 @@
 import discord
-from discord.ext import commands,tasks
+from discord.ext import commands
 import requests
-# import json
-import nhentaiCrawer
-import nhentaiEmbed
-
-# with open('setting.json', mode='r',encoding='utf8') as jfile:
-    # jdata = json.load(jfile)
+import threading
+from bs4 import BeautifulSoup
 
 intents = discord.Intents.all()
-bot=commands.Bot(command_prefix="[",intents=intents)
+bot=commands.Bot(command_prefix=".",intents=intents)
 
 @bot.event
 async def on_ready():
@@ -19,28 +15,50 @@ async def on_ready():
 
 ##################################################################################################
 
+urls = []
+
+def crawlerPage(number,page):
+    print(f"第{page}圈爬蟲")
+    url = f"https://nhentai.net/g/{number}/{page}/"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362'}
+    r = requests.get(url,headers=headers)
+    Burl = BeautifulSoup(r.text, 'html.parser')
+    img_tags = Burl.find_all('img')
+    for tag in img_tags:
+        imgUrl = tag.get('src')
+    if imgUrl == "https://static.nhentai.net/img/logo.090da3be7b51.svg":
+        return False #爬到logo代表已經結束了
+    else:
+        urls.append(imgUrl)
+
+def crawlerLoop(number):
+    threads = [] #宣告一個list用來放多線程
+    page = 1
+    while 1:
+        threads.append(threading.Thread(target = crawlerPage, args=(number,page)))
+        if crawlerPageIndex == False:
+            break #第30行回傳False代表爬完了就break
+        crawlerPageIndex = threads[page].start()
+        page += 1
+
 @bot.command()
 async def n(ctx,number=None,page=0):
     c = ctx.channel.is_nsfw()
     if c == False:
         await ctx.send("This is not NSFW channel")
     else:
-        urls = []
-            
         if number!=None:
             #爬蟲
             #從內容頁面
-            urls,url = nhentaiCrawer.crawler(number,page)
-            print(urls,url)
+            crawlerLoop(number)
 
-            page = 0
+            page = 1
             #輸出訊息
             print("輸出訊息")
-            embed = discord.Embed
-            nhentaiEmbed.Embed(url=url,urls=urls,page=page)
+            embed=discord.Embed(color=0x009dff,title="Nhentai Viewer")
+            embed.set_footer(text="By Young#0001")
             embed.set_image(url=urls[0])
-            #增加表情符號
-            message = await ctx.send(embed=embed)
+            message=await ctx.send(embed=embed)
             for i in ["◀","▶"]:
                 await message.add_reaction(i)
             #檢查表情符號(函式)
@@ -49,10 +67,9 @@ async def n(ctx,number=None,page=0):
                 return user == ctx.author and reaction.message == message
             #檢查表情符號(迴圈)
             while 1 :
-                print("檢查表情符號(迴圈)")
-                if(page + 1 > len(urls) - 1):
-                    print("已結束")
-                    nhentaiEmbed.EmbedEnd()
+                if(page > len(urls) - 1):
+                    embed=discord.Embed(color=0x009dff,title="Nhentai Viewer",description="The end.")
+                    embed.set_footer(text="By Young#0001")
                     await message.edit(embed=embed)
                     break
                 reaction, user = await bot.wait_for("reaction_add",timeout=60.0,check=check)
@@ -61,12 +78,12 @@ async def n(ctx,number=None,page=0):
                 elif str(reaction) == "◀":
                     page-=1
                 await message.remove_reaction(reaction,user)
-                nhentaiEmbed.Embed(url=url,urls=urls,page=page)
+                embed=discord.Embed(color=0x009dff,title="Nhentai Viewer")
+                embed.set_footer(text="By Young#0001")
+                embed.set_image(url=urls[page])
                 await message.edit(embed=embed)
         else:
             await ctx.send(f"Please input number")
 
 ##################################################################################################
-# bot.run(jdata['TestBotTOKEN'])
-
-bot.run("Nzg2OTg2OTU0NDc4NTgzODU4.X9OYtw.VSF93Uq1j1dqdleC-n1GiDaELvA")
+bot.run('token here')
